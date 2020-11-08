ffaabd3e3207c16b69034a0316b27338e6e14c4e
issamBoutissante <boutissante.issam.dev@gmail.com>
2020-11-08 18:26:04 +0100

    first asp app

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using hello.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hello.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { name="detachment",id=1};
+            return View(movie);
+        }
+    }
+}
